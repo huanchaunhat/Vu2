1b57f6fa3fadaffd1498bf81c2b2249b3c03772c
nhathuan <huanchaunhat@gmail.com>
2025-12-16 21:53:50 +0700

    debug

--- public/index.php
+++ public/index.php
@@ -1,5 +1,8 @@
 <?php
 // public/index.php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 session_start(); // dùng session lưu thông tin đăng nhập
 
